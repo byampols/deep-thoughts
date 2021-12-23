518be29ef99e625fa2295ae036c50821fbbef941
unknown <byampols@alumni.cmu.edu>
2021-12-22 20:11:31 -0500

    added ability to post/reply

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,8 +3,10 @@ import {useParams} from 'react-router-dom';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth';
 
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
 import { Link } from 'react-router-dom';
 
@@ -38,6 +40,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
